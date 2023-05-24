bda1c5955af88730013a8f1c0269659148ad2d84
djebbyy <105248196+djebbyy@users.noreply.github.com>
2023-05-23 20:21:26 -0400

    Add files via upload

---
+++ ecommerce/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+declare var $:any;
+declare function initpageecommerce([]):any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ecommerce';
+  constructor(){
+    setTimeout(() => {
+     initpageecommerce($);
+    }, 50);
+  }
+}
